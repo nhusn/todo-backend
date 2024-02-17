d7821e98c7a2a65f9c669062aa6d7084750b7a9f
nhusn <139863367+nhusn@users.noreply.github.com>
2024-02-17 15:04:09 +0530

    minor updates

--- router.js
+++ router.js
@@ -9,20 +9,28 @@ router.get("/message",(req,res)=>{
     return res.status(200).json(num)
 });
 
+// add to list
 router.post("/add",toDoController.addToListController)
 
+// delete from todo
 router.post('/delete',toDoController.deleteToDoController)
 
+// update item from list
 router.post('/update',toDoController.updateToDoController)
 
+// get all list 
 router.get('/all-todos',toDoController.getAllToDoController)
 
+// get list by date
 router.get('/getbydate',toDoController.getToDoByDateController)
 
+// update status of list item
 router.post('/status-update',toDoController.statusUpdaterController)
 
+// get list by limit 5
 router.post('/pagination',toDoController.limitToDoContoller)
 
+// get todo by capitalised text
 router.post('/get-one-todo',toDoController.getOneToDoController)
 
 module.exports = router
